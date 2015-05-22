2db2e0e346bbbbd83fc1bff5f18831c59ca00622
Larry D Gardner <ldgalaxy@comcast.net>
2015-05-22 13:13:04 -0600

    First part of turotial on creating forms using the GET method

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+
+?>
+
+
+<form action="index.php" method="GET">
+   Day:  <br><input type="text" name="day"><br>
+   Date: <br><input type="text" name="date"><br>
+   Year: <br><input type="text" name="year"><br><br>
+   <input type="submit" value="Sibmit">
+
+</form>
