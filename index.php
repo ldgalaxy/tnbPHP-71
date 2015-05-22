0d6272b5791e4b8ff1d8ecad8cc3d919aeacf2dd
Larry D Gardner <ldgalaxy@comcast.net>
2015-05-22 16:12:11 -0600

    Tutorial 72 Using htmlentities for security

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
 
+if(isset($_GET['day']) && isset($_GET['date']) && isset($_GET['year'])){
+   $day =  htmlentities($_GET['day']);
+   $date = htmlentities($_GET['date']);
+   $year = htmlentities($_GET['year']);
 
+   if(!empty($day) && !empty($date) && !empty('year')){
+      echo 'It is ' . $day . ' ' . $date . ' ' . $year; 
+   }else{
+      echo 'Fill in all fields.';      
+   }
+}
 
 
 ?>
@@ -10,6 +20,6 @@
    Day:  <br><input type="text" name="day"><br>
    Date: <br><input type="text" name="date"><br>
    Year: <br><input type="text" name="year"><br><br>
-   <input type="submit" value="Sibmit">
+   <input type="submit" value="Submit">
 
 </form>
